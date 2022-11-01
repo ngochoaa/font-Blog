51deecbb78eda53551f7197919bacd22b695fd71
Kênh Lập Trình Website <nguyenminhduc2001pt@gmail.com>
2022-11-01 16:46:52 +0700

    html users completed

--- js/user.js
+++ js/user.js
@@ -1,5 +1,20 @@
 $(document).ready(function () {
-    $.get("http://localhost:5243/api/User", function(data, status){
-        console.log("Data: " + data[1].displayName + "\nStatus: " + status);
+    $.get("http://localhost:5243/api/User", function (data, status) {
+        console.log("Data: " + data[0].displayName + "\nStatus: " + status);
+        data.forEach(user => {
+            var html = `<tr>
+            <td>${user.displayName}</td>
+            <td>${user.email}</td>
+            <td>${user.phone}</td>
+            <td>${user.dateOfBirth}</td>
+            <td>${user.address}</td>
+            <td> <button>Edit User</button></td>
+            <td><button>Delete User</button></td>
+            </tr>`
+            
+            var emlementListUser = document.querySelector("#listUsers");
+            emlementListUser.innerHTML += html
+        });
+        
     });
 });
